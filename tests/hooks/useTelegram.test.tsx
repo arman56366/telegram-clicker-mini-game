062d807c099d064a1926da92cf19dc64ed6667ad
arman56366 <156896371+arman56366@users.noreply.github.com>
2026-02-13 15:45:23 +0400

    Delete tests directory

--- tests/hooks/useTelegram.test.tsx
+++
@@ -1,9 +0,0 @@
-import { renderHook } from "@testing-library/react-hooks"
-import { useTelegram } from "../../src/hooks/useTelegram"
-
-describe("useTelegram Hook", () => {
-  it("should return mock user in dev mode", () => {
-    const { result } = renderHook(() => useTelegram())
-    expect(result.current.user?.first_name).toBe("Test")
-  })
-})
